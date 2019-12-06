e5013d29fc533924c36ad7dd03c8b0020af10f99
KPVBY <kpvbygm@gmail.com>
2019-12-06 16:44:10 +0300

    mainComponent

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, {Component} from 'react';
 import './App.css';
 import Header from "./components/header";
 import Footer from "./components/footer";
+import Main from "./components/main";
 
 export default class App extends Component {
   render() {
     return (
         <div className="app">
           <Header/>
-          <main>Main</main>
+          <Main/>
           <Footer/>
         </div>
     );
---
+++ src/components/main.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+export default class Main extends Component {
+    render() {
+        return (
+            <main className="app-main">
+                <div className="main-area">
+                    Area
+                </div>
+            </main>
+        )
+    }
+}
